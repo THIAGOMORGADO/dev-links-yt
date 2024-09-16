2693fc87444e75bbcf43ff5edf5683f693f9ecf8
thiago morgado <thiago_morgado@hotmail.com>
2024-09-16 01:45:31 -0300

    feat/ details page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeContextProvider } from "./context/ThemeContext.tsx";
+import Details from "./page/Details/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeContextProvider>
-    <App />
+    {/* <App /> */}
+    <Details />
   </ThemeContextProvider>
 );
---
+++ src/page/Details/index.tsx
@@ -0,0 +1,69 @@
+import { useTheme } from "../../hooks/useTheme";
+import { ArrowLeft, LinkSimpleHorizontal } from "@phosphor-icons/react";
+
+import Moon from "../../assets/moon.svg";
+import Sun from "../../assets/sun.svg";
+
+import justaImg from "../../assets/justa.png";
+
+export default function Details() {
+  const { theme, setTheme } = useTheme();
+
+  function handleChangeTheme() {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }
+  return (
+    <div className="">
+      <header className=" h-[62px] p-5   flex items-center justify-between">
+        <button className="transition-all 500 hover:scale-110">
+          <ArrowLeft
+            weight="bold"
+            size={22}
+            className="text-dark dark:text-white"
+          />
+        </button>
+        <h2 className="font-bold text-lg text-dark dark:text-white ">
+          Detalhes do projeto
+        </h2>
+
+        <button onClick={handleChangeTheme}>
+          {theme !== "dark" ? (
+            <img width={22} height={22} src={Moon} />
+          ) : (
+            <img width={22} height={22} src={Sun} />
+          )}
+        </button>
+      </header>
+      <main className="p-5 flex flex-col gap-3">
+        <div className="flex flex-col items-center gap-5">
+          <img src={justaImg} />
+        </div>
+        <div className="flex justify-center items-center gap-3">
+          <h3 className="text-dark  dark:text-white font-bold text-lg">
+            Justa Troca
+          </h3>
+          <a href="#" target="_blank" rel="noopener noreferrer" className="">
+            <LinkSimpleHorizontal size={22} weigth="bold" />
+          </a>
+        </div>
+        <div className="text-[#858585] dark:text-[#eee]">
+          O aplicatiovo justa trpca tem como objetivo facilitar a troca de nems
+          matteriais ou serviços
+        </div>
+
+        <div className="">
+          <h1>Habilidades</h1>
+          <div className="">
+            <ul className="list-disc list-inside">
+              <li>React</li>
+              <li>Next.js</li>
+              <li>Tailwind CSS</li>
+              <li>Node.js</li>
+              <li>PostgreSQL</li>
+            </ul>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
